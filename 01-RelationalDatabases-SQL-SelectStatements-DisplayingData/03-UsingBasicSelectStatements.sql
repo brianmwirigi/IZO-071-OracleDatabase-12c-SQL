1e768240117eaf87718af4e6a02fe5ed18feec21
brianmwirigi <bmmaingi17@gmail.com>
2026-07-28 20:19:42 +0300

    03

--- 01-RelationalDatabases-SQL-SelectStatements-DisplayingData/03-UsingBasicSelectStatements.sql
+++ 01-RelationalDatabases-SQL-SelectStatements-DisplayingData/03-UsingBasicSelectStatements.sql
@@ -1,3 +1,24 @@
 show user;
 
 select * from employees;
+
+show user;
+
+select * from employees;
+
+select count(*) from employees;            
+
+SELECT USER FROM DUAL;
+
+SHOW USER;
+
+SELECT USER FROM DUAL;
+
+DESC employees;
+
+SELECT employee_id, first_name, last_name FROM employees;
+
+
+
+
+
