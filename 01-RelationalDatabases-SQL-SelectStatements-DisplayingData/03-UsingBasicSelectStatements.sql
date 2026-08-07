08e2149ac9d47e4715856e241514045eaeaf2534
brianmwirigi <bmmaingi17@gmail.com>
2026-08-07 19:49:32 +0300

    03

--- 01-RelationalDatabases-SQL-SelectStatements-DisplayingData/03-UsingBasicSelectStatements.sql
+++ 01-RelationalDatabases-SQL-SelectStatements-DisplayingData/03-UsingBasicSelectStatements.sql
@@ -36,6 +36,93 @@ select Employee_ID, First_name, Last_name from employees;
 
 select first_name,last_name,employee_ID
 from employees;
+ 
+ 
+ 
+-- 03 -> Arithmetic Expressions
+-- use of arithmetic operations in a SELECT statement(using when one has a numeric column)
+-- four basic arithmetic operations are multiply, divide, add,subtract
+
+-- using arithmeitc operations in a select statement
+
+-- multiply the salary column by 12 to get an annual salary
+--(derived column /calculated field) -> salary*12
+select employee_id, first_name, Last_name, salary, salary * 12
+from employees;
+
+-- aivide by 4 to get a weekly salary
+select employee_id , first_name, last_name, salary ,salary / 4 
+from employees;
+
+-- add or subtract a value to estimate raise oe deduction
+select employee_id, first_name, last_name, salary, salary + 500
+from employees;
+
+select employee_id, first_name, last_name, salary, salary - 500
+from employees;
+
+
+-- 04 -> operator precedence in arithmetic expressions
+-- order of operations when calculations are combined
+-- multiply-divide-add-subtract
+-- alter default order of operation by using parentheses ()
+-- using arithmetic operations in a select statement
+
+-- alter the default order of operations by using parenthesis
+select employee_id, first_name,Last_name, salary, salary * 12 + 500 from employees;
+
+--functionally equivalent to the statement above, but clarifies the order (parenthesis ensures the operation in the parenthesis is done/implemented first)
+select employee_id, first_name, Last_name, salary, (salary * 12) + 500 from employees;
+
+--this statement performs the calculation of 500*12 first(equals 6000), then adds 6000 to the salary
+select employee_id, first_name, last_name, salary ,salary + 500 * 12 from employees;
+
+--this statement adds 500 o the monthly salary before multiplyin by 12(make sure the first operation is implemented first by placing them in parenthesis)
+select employee_id ,first_name, last_name, salary , (salary +500) * 12 from employees;
+
+
+
+-- 05 -> NULL value
+-- presence of null. it is nothing. no valu ever entered
+-- it does not indicate a value was entered then deleted, does not equate to zero or any other value (including other nulls)
+-- null does not equal another null, its simply nothing
+-- using arithmetic operations in a select statement
+
+-- be mindful of nulls when performing calculations. 
+select employee_id, first_name, last_name, salary, commission_pct from employees;
+
+select employee_id, first_name,last_name, salary, commission_pct, salary*commission_pct from employees;
+
+
+
+-- 06 -> column aliases to make results a little more meaningful
+-- presenting the information to an end user through reports or saved view
+-- name of any given column in any given table is set when the table is designed. 
+-- not recommended to change the name of a column
+-- using column aliases to make results more meaningful
+
+-- without aliases, column names appear in the result as entered in the select statement
+select employee_id, Last_name, salary ,salary * 12 from employees;
+
+--using alias to prevent the reuslts ina more user-friendly manner
+--use double quote to define the alias, the use of AS keyword is optional
+select employee_id, last_name AS "name", salary "monthly salary",salary * 12"annual salary" from employees;
+
+
+
+-- 07 -> concatenation operator
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
